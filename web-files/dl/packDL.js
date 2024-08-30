c4e4d0b3b63db9b245b75cb0e6757433da1935b8
KayoticCarnige <kayos156official@gmail.com>
2024-08-30 15:30:54 -0400

    idk what to put here

---
+++ web-files/dl/packDL.js
@@ -0,0 +1,23 @@
+let vList;
+fetch("https://iatj-modpack.github.io/meta.json", {
+    method: "GET",
+    headers: {
+        Accept: "application/json",
+    },
+})
+    .then((response) => response.json())
+    .then((response) => {
+        console.log("Fetched!")
+        var vList = response;
+        if (document.location.pathname == "/dl/0.0.0") {
+            document.location = "https://iatj-modpack.github.io/versions/0.0.0/iatj_modpack_file_0.0.0.mrpack"
+        } else {
+            for (var i = 0; i < vList.versions.length; i++) {
+                if (document.location.pathname == "/dl/" + vList.versions[i].id) {
+                    console.log(vList.versions[i].id)
+                    document.location = vList.versions[i].mpfile
+                }
+            }
+        }
+    })
+
