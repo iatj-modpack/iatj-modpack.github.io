e30e79ab60623d8ab17719a19a7a856a26b4ae24
KayoticCarnige <kayos156official@gmail.com>
2024-08-30 01:48:42 -0400

    ?

--- web-files/dl/downloadMPFile.js
+++ web-files/dl/downloadMPFile.js
@@ -8,13 +8,14 @@ fetch("/iatj/meta.json", {
     .then((response) => response.json())
     .then((response) => (vList = response))
     .then(() => {
-        for (var i = 0; i < vList.versions.length; i++) {
-            if (document.location.pathname == vList.versions[i].id) {
-                console.log(vList.versions[i].id)
-                document.location = vList.versions[i].mpfile
-            } else if (i == 0 && document.location.pathname == "/iatj/dl/latest") {
-                document.location = `https://kckarnige.github.io/iatj/versions/${vList.versions[vList.versions.length].id}/iatj_modpack_file_${vList.versions[vList.versions.length].id}.mrpack`
-            } else if (i == 0 && document.location.pathname == "/iatj/dl/0.0.0") {
+        var latestV = vList.versions.length;
+        for (var currentV = 0; currentV < latestV; currentV++) {
+            if (document.location.pathname == vList.versions[currentV].id) {
+                console.log(vList.versions[currentV].id)
+                document.location = vList.versions[currentV].mpfile
+            } else if (currentV == 0 && document.location.pathname == "/iatj/dl/latest") {
+                document.location = `https://kckarnige.github.io/iatj/versions/${vList.versions[latestV].id}/iatj_modpack_file_${vList.versions[latestV].id}.mrpack`
+            } else if (currentV == 0 && document.location.pathname == "/iatj/dl/0.0.0") {
                 document.location = "https://kckarnige.github.io/iatj/versions/0.0.0/iatj_modpack_file_0.0.0.mrpack"
             } else {
                 document.location = "https://kckarnige.github.io/iatj"
