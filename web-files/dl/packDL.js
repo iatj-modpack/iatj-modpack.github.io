aeb142b0395ede9df0befb0ba44b6094f5327ffb
KayoticCarnige <kayos156official@gmail.com>
2024-08-30 20:51:51 -0400

    Update packDL.js

--- web-files/dl/packDL.js
+++ web-files/dl/packDL.js
@@ -16,17 +16,13 @@ fetch("https://iatj-modpack.github.io/meta.json", {
                 console.log(vList.versions[i].id)
                 document.location = vList.versions[i].mpfile
             }
+            if (document.location.pathname == "/dl/latest" && i == vList.versions.length) {
+                console.log(vList.versions[i].id)
+                document.location = vList.versions[i].mpfile
+            }
         }
         if (document.location.pathname == "/dl/0.0.0") {
             document.location = "https://iatj-modpack.github.io/versions/0.0.0/iatj_modpack_file_0.0.0.mrpack"
-        }
-        if (document.location.pathname == "/dl/latest") {
-            async function YES() {
-                console.log(vList.versions[vListSize])
-                var latestMPFile = await vList.versions[vListSize].mpfile;
-                document.location = latestMPFile;
-            }
-            YES()
         }
     })
 
