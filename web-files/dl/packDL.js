ada8a387f1e72025365be957c5f7c3e91c74c213
KayoticCarnige <kayos156official@gmail.com>
2024-08-31 14:07:00 -0400

    Change page icon when download pack

--- web-files/dl/packDL.js
+++ web-files/dl/packDL.js
@@ -1,6 +1,11 @@
 let vList;
 let vListSize;
 let dlPage;
+function setForDL () {
+    dlPage = true;
+    document.title = "Getting download..";
+    document.getElementById("pageIco").setAttribute("href", "https://iatj-modpack.github.io/dlicon.ico");
+}
 fetch("https://iatj-modpack.github.io/meta.json", {
     method: "GET",
     headers: {
@@ -13,20 +18,17 @@ fetch("https://iatj-modpack.github.io/meta.json", {
         vList = response;
         for (var i = 0; i < vList.versions.length; i++) {
             if (document.location.pathname == "/dl/" + vList.versions[i].id) {
-                dlPage = true;
-                document.title = "Getting download..";
+                setForDL();
                 console.log(vList.versions[i].id)
                 document.location = vList.versions[i].mpfile
             }
         }
         if (document.location.pathname == "/dl/0.0.0") {
-            dlPage = true;
-            document.title = "Getting download..";
+            setForDL();
             document.location = "https://iatj-modpack.github.io/versions/0.0.0/iatj_modpack_file_0.0.0.mrpack"
         }
         if (document.location.pathname == "/dl/latest") {
-            dlPage = true;
-            document.title = "Getting download..";
+            setForDL();
             fetch("https://iatj-modpack.github.io/versions/latest/vnum.txt", {
                 method: "GET",
                 headers: {
