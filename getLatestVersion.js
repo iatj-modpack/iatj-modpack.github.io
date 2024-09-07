782ddc99d8f8c312070a3cfb36641c4c1abc4243
KayoticCarnige <kayos156official@gmail.com>
2024-09-07 01:27:52 -0400

    Even dumber

--- getLatestVersion.js
+++ getLatestVersion.js
@@ -1,29 +1,17 @@
-// https://brianchildress.co/find-latest-file-in-directory-in-nodejs/
-const path = require('path');
-const fs = require('fs');
-
-const getMostRecentFile = (dir) => {
-  const files = orderRecentFiles(dir);
-  return files.length ? files[0] : undefined;
-};
-
-const orderRecentFiles = (dir) => {
-  return fs.readdirSync(dir)
-    .filter((file) => fs.lstatSync(path.join(dir, file)).isDirectory())
-    .map((file) => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
-    .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
-};
-
-//https://stackoverflow.com/a/58027214
-var data = [getMostRecentFile('./versions')],
-    highest = data
-        .reduce((a, b) =>
-            0 < a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: 'base' })
-                ? a
-                : b
-        );
-
-console.log(highest);
-
-fs.cpSync(path.join(__dirname,"versions",highest.file), path.join(__dirname,"versions","latest"), {recursive: true, force: true});
-
+fetch("https://raw.githubusercontent.com/iatj-modpack/iatj-modpack.github.io/main/meta.json", {
+  method: "GET",
+  headers: {
+      Accept: "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((response) => {
+      console.log("Fetched!")
+      vList = response;
+      for (var i = 0; i < vList.versions.length; i++) {
+        var vListLength = vList.versions.length+-1;
+        if (i == vListLength) {
+            fs.cpSync(path.join(__dirname,"versions",vList.versions[i].id), path.join(__dirname,"versions","latest"), {recursive: true, force: true});
+        }
+    }
+  })
