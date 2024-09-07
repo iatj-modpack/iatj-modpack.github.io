30026fceabcc03bcd5e90fc9cc1319e0965ca8b3
KayoticCarnige <kayos156official@gmail.com>
2024-09-07 00:45:03 -0400

    Hopefully this works now

--- getLatestVersion.js
+++ getLatestVersion.js
@@ -3,16 +3,27 @@ const path = require('path');
 const fs = require('fs');
 
 const getMostRecentFile = (dir) => {
-  const files = orderReccentFiles(dir);
+  const files = orderRecentFiles(dir);
   return files.length ? files[0] : undefined;
 };
 
-const orderReccentFiles = (dir) => {
+const orderRecentFiles = (dir) => {
   return fs.readdirSync(dir)
     .filter((file) => fs.lstatSync(path.join(dir, file)).isDirectory())
     .map((file) => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
     .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
 };
 
-console.log(getMostRecentFile('./versions').file);
-fs.cpSync(path.join(__dirname,"versions",getMostRecentFile('./versions').file), path.join(__dirname,"versions","latest"), {recursive: true});
+//https://stackoverflow.com/a/58027214
+var data = [getMostRecentFile('./versions')],
+    highest = data
+        .reduce((a, b) =>
+            0 < a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: 'base' })
+                ? a
+                : b
+        );
+
+console.log(highest.file);
+
+fs.cpSync(path.join(__dirname,"versions",highest.file), path.join(__dirname,"versions","latest"), {recursive: true, force: true});
+
